74326f3e8b7d1f4d15e500a8f66dfd134fc1203a
edinson <edinson@engineer>
2014-04-21 20:31:32 -0500

    Project Structure for information data: Places and danger places.

---
+++ www/libs/data/information.js
@@ -0,0 +1,94 @@
+var info = {
+    markers: {
+        me: null,
+        points: []
+    },
+    
+    shapes: [],
+    
+    places: [
+        {
+            name: 'Restaurant',
+            markers: [
+                {
+                    latlng: new google.maps.LatLng(11.008216, -74.817706),
+                    name: 'Zushi Zone',
+                    description: '',
+                    image: '',
+                    punctuation: 4.5
+                }, {
+                    latlng: new google.maps.LatLng(11.007689, -74.817277),
+                    name: 'Habana Blue',
+                    description: '',
+                    image: '',
+                    punctuation: 4
+                }, {
+                    latlng: new google.maps.LatLng(11.006699, -74.812964),
+                    name: 'Rodizio',
+                    description: '',
+                    image: '',
+                    punctuation: 4
+                }, {
+                    latlng: new google.maps.LatLng(11.006152, -74.826482),
+                    name: 'Arabe Internacional',
+                    description: '',
+                    image: '',
+                    punctuation: 4.22
+                }, {
+                    latlng: new google.maps.LatLng(11.001328, -74.817749),
+                    name: 'Quile Parrilla',
+                    description: '',
+                    image: '',
+                    punctuation: 4.3
+                }
+            ]
+        }, {
+            name: 'Hotels',
+            markers: []
+        }, {
+            name: 'Malls',
+            markers: []
+        }, {
+            name: 'Hospitals',
+            markers: []
+        }, {
+            name: 'Discos',
+            markers: []
+        }, {
+            name: 'Parks',
+            markers: []
+        }, {
+            name: 'Museums',
+            markers: []
+        }, {
+            name: 'Colleges',
+            markers: []
+        }, {
+            name: 'Cinemas',
+            markers: []
+        }, {
+            name: 'Pet Care',
+            markers: []
+        }, {
+            name: 'Teathers',
+            markers: []
+        }, {
+            name: 'Fuels',
+            markers: []
+        }, {
+            name: 'Drugstores',
+            markers: []
+        }, {
+            name: 'Strip Clubs',
+            markers: []
+        }
+    ],
+    
+    danger: [
+        {
+            latlng: new google.maps.LatLng(10.967520, -74.775138),
+            radius: 550,
+            opacity: 0.8
+        }
+    ]
+};
